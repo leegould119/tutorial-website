5eede32f86b0877e462dd423d6e49d417fa59c3d
Lee <leegould119@gmail.com>
2020-06-18 10:05:35 +0200

    lees awesome changes.

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ const express = require("express");
 
 const app = express();
 
-const math = (1+1);
+const math = 1 + 1;
 
 //Nathalie made this
- const math = (2-2); 
+const math = 2 - 2;
+
+const test = { type: "this is an object" };
+const test1 = 2.2 + 1;
